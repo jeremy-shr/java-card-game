439ad0be927213e8ef50a7c9299e36b0f08f87b5
Stickman230 <123313783+Stickman230@users.noreply.github.com>
2023-11-14 17:53:19 +0000

    Update Pack.java
    - added the scanner import for user input
    - created constructor and pack object 
    - added get methods

--- Cards/Pack.java
+++ Cards/Pack.java
@@ -1,3 +1,35 @@
+import java.util.Scanner;
+
 public class Pack {
+    private int nbOfPlayers;
+    private String packFile;
+
+    public Pack(int n,String location) {
+        this.nbOfPlayers = n;
+        this.packFile = location;
+    }
     
+    
+    public static void main(String[] args){
+        
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter the number of players:");   
+        int nbOfPlayers = userInput.nextInt();
+    
+        Scanner userInput2 = new Scanner(System.in);
+        System.out.println("Please enter location of pack to load:");   
+        String packFile = userInput2.nextLine();
+       
+        Pack myPack = new Pack(nbOfPlayers, packFile);
+    
+    }
+    
+    public int getNumberOfPlayers(){
+        return nbOfPlayers;
+    }
+    
+    public String getPackFile(){
+        return packFile;
+    }
+
 }
