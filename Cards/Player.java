ee240f63a50cb7079c0f47c7acca0f4cad646bad
Stickman230 <123313783+Stickman230@users.noreply.github.com>
2023-11-21 00:28:01 +0000

    Update Player.java

--- Cards/Player.java
+++ Cards/Player.java
@@ -1,4 +1,8 @@
 import java.util.concurrent.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.util.Iterator;
 import java.util.ArrayList;
 
 public class Player implements Runnable{
@@ -35,5 +39,39 @@ public class Player implements Runnable{
     public void startThread(){
         Thread thread = new Thread(this);
         thread.start();  
+    }
+
+    public Boolean winner(){
+        Card firstCard = playerHand.peek();
+        String firstCardVal = firstCard.getFaceValue();
+        Iterator<Card> iterator = this.getPlayerHand().iterator();
+
+        while (iterator.hasNext()) {
+            Card currentCard = iterator.next();
+            String currentCardVal = currentCard.getFaceValue();
+            if (!currentCardVal.equals(firstCardVal)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void createOutputFile(){
+        String pathName = "player"+ getPlayerNum() +"_output.txt";
+        Iterator<Card> iterator = this.getPlayerHand().iterator();
+        ArrayList<String> handArray = new ArrayList<>(); 
+        while (iterator.hasNext()) {
+            String currentCard = iterator.next().getFaceValue();
+            handArray.add(currentCard);
+        }
+        try{
+            File myFile = new File(pathName);
+            myFile.createNewFile();
+            FileWriter myWriter = new FileWriter(pathName);
+            myWriter.write("player "+getPlayerNum()+" initial hand "+handArray.get(0)+" "+handArray.get(1)+" "+handArray.get(2)+" "+handArray.get(3));
+            myWriter.close();
+        } catch (IOException e){
+            System.out.println("File has an error regagding IO ");
+        }
     }
 }
