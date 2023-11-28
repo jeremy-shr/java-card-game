5a07f1e5618b2edf25877aaa475cdfea67636f50
Stickman230 <123313783+Stickman230@users.noreply.github.com>
2023-11-28 00:09:14 +0000

    Add files via upload

---
+++ testing/Card.java
@@ -0,0 +1,13 @@
+public class Card {
+    String faceValue;
+
+    // Card constructor
+    public Card(String val) {
+        this.faceValue = val;
+    }
+
+    // Define get method used for card preference and comparison
+    public String getFaceValue() {
+        return faceValue;
+    }
+}
